ce34d3a3eef4a4f408a220dc27e48c5f58e16359
Thangkv <thangkvth2203028@fpt.edu.vn>
2022-08-22 17:44:26 +0700

    update 22/08/2022

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const conn = mysql.createConnection({
     port: "3306",
     user: "root",
     password: "",
-    database: "project_light",
+    database: "eproject_v1",
+    // database: "eprojectlight",
+    // database: "project_light",
     multipleStatements: true
 });
 // var stack0 = null;
@@ -62,6 +64,7 @@ app.get("/trangchu",function (req,res) {
     })
 
 })
+
 app.get("/test",function (req,res) {
     count++;
     /// truy van dc db để lấy dữ liệu sản phẩm
@@ -109,7 +112,7 @@ app.get("/test1",function (req,res) {
 
 app.get("/danhsachsanpham",function (req,res) {
 
-    /// truy van dc db để lấy ds sinh viên
+
     const sql_txt = "SELECT * FROM `products`";
     conn.query(sql_txt,function (err,data) {
         if(err) res.send("404 not found");
@@ -125,8 +128,7 @@ app.get("/danhsachsanpham",function (req,res) {
 
 app.get("/dentrangtri",function (req,res) {
 
-    /// truy van dc db để lấy ds sinh viên
-    const sql_txt = "SELECT * FROM `products` WHERE CategoryID=2;";
+    const sql_txt = "SELECT * FROM `products` WHERE CategoryID=1";
     conn.query(sql_txt,function (err,data) {
         if(err) res.send("404 not found");
         else{
@@ -136,52 +138,219 @@ app.get("/dentrangtri",function (req,res) {
             });
         }
     })
-
 });
-
-app.get("/products-detail",function (req,res) {
-    // phải tìm cách lấy đc giá trị tham số trên url
-    var PID = req.query.PID;
-
-    const sql_txt = "select * from products where PID like '" +PID+"';"
-
-    conn.query(sql_txt,function (err,data) {
-        if(err) res.send("Error");
-        else{
-
-                var products = data;
-                // res.send(data);
-                res.render("product_detail",{
-                    "products":products
-                });
-        }
-    })
-
-})
-app.get("/danhsachsanpham_duoi500k",function (req,res) {
-
-    /// truy van dc db để lấy ds sinh viên
-    const sql_txt = "SELECT * FROM `products` WHERE Price < 500000;";
+app.get("/denoptran",function (req,res) {
+    const sql_txt = "SELECT * FROM `products` WHERE CategoryID=3";
     conn.query(sql_txt,function (err,data) {
         if(err) res.send("404 not found");
         else{
             var products = data;
-            res.render("danhsachsanpham_duoi500k",{
+            res.render("denoptran",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/denchum",function (req,res) {
+        const sql_txt = "SELECT * FROM `products` WHERE CategoryID=4";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("denchum",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/dencay",function (req,res) {
+    const sql_txt = "SELECT * FROM `products` WHERE CategoryID=5";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dencay",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/dentha",function (req,res) {
+    const sql_txt = "SELECT * FROM `products` WHERE CategoryID=6";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentha",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/phillips",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=1";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/osram",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=2";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/cree",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=3";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/nichia",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=4";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/panasonic",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=5";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/zumtobel",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=6";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+app.get("/cavani",function (req,res) {
+    const sql_txt = "select *,brands.Name as thuonghieu from products INNER JOIN brands ON products.BrandID = brands.BID WHERE BrandID=7";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("dentheothuonghieu",{
+                "products":products
+            });
+        }
+    })
+});
+// app.get("/products-detail",function (req,res) {
+//
+//     var PID = req.query.PID;
+//     var xxxx = req.query.xxxx;
+//     const sql_txt = "select * from products where PID like '" +PID+"';"
+//
+//     conn.query(sql_txt,function (err,data) {
+//         if(err) res.send("Error");
+//         else{
+//             var products = data;
+//             var products1 = data;
+//                 // res.send(data);
+//                 res.render("product_detail2",{
+//                     xxxx:xxxx,
+//                     "products":products,
+//                     "products1":products1
+//
+//                 });
+//         }
+//     })
+//
+// })
+app.get("/products-detail",function (req,res) {
+
+    var PID = req.query.PID;
+    var xxxx = req.query.xxxx;
+    // const sql_txt = "select * from products where PID like '" +PID+"';\n" + "select * from products INNER JOIN categorys ON products.CategoryID = categorys.CaID WHERE categorys.CaID = (SELECT products.CategoryID FROM products WHERE products.PID LIKE '"+PID+"');"
+    const sql_txt = "select Products.*,Brands.BID,Brands.Name as thuonghieu,Brands.Origin from Products INNER JOIN Brands ON Products.BrandID = Brands.BID where PID like '" +PID+"';\n" + "select * from products INNER JOIN categorys ON products.CategoryID = categorys.CaID WHERE categorys.CaID = (SELECT products.CategoryID FROM products WHERE products.PID LIKE '"+PID+"');"
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("Error");
+        else{
+            var products = data[0];
+            var products1 = data[1];
+            // res.send(data);
+            res.render("product_detail2",{
+                xxxx:xxxx,
+                "products":products,
+                "products1":products1
+
+            });
+        }
+    })
+
+})
+app.get("/danhsachsanpham_duoi5000k",function (req,res) {
+
+    const sql_txt = "SELECT * FROM `products` WHERE Price < 5000000;";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("danhsachsanpham_duoi5000k",{
                 "products":products
             });
         }
     })
 
 });
-app.get("/danhsachsanpham_tren500k",function (req,res) {
+app.get("/danhsachsanpham_tu5000k-10000k",function (req,res) {
 
-    /// truy van dc db để lấy ds sinh viên
-    const sql_txt = "SELECT * FROM `products` WHERE Price >= 500000;";
+    const sql_txt = "SELECT * FROM `products` WHERE Price >= 5000000 and Price < 10000000;";
     conn.query(sql_txt,function (err,data) {
         if(err) res.send("404 not found");
         else{
             var products = data;
-            res.render("danhsachsanpham_tren500k",{
+            res.render("danhsachsanpham_tu5000k-10000k",{
+                "products":products
+            });
+        }
+    })
+
+});
+app.get("/danhsachsanpham_tren10000k",function (req,res) {
+
+     const sql_txt = "SELECT * FROM `products` WHERE Price >= 10000000;";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            res.render("danhsachsanpham_tren10000k",{
                 "products":products
             });
         }
@@ -197,7 +366,7 @@ app.get("/products-search",function (req,res) {
         if(err) res.send("Error");
         else{
             var products = data;
-            res.render("test2",{
+            res.render("ketquatimkiem",{
                 "products":products
             });
         }
@@ -222,3 +391,96 @@ app.get("/gioithieu",function (req,res) {
     // })
 
 })
+
+app.get("/lienhe",function (req,res) {
+
+    res.render("lienhe", {});
+});
+app.get("/giohang",function (req,res) {
+
+    res.render("giohang",{
+        });
+
+})
+app.get("/thongtinvanchuyen",function (req,res) {
+
+    res.render("thongtinvanchuyen",{
+    });
+
+});
+app.get("/dangky",function (req,res) {
+
+    res.render("dangky",{
+    });
+
+});
+
+app.get("/dangnhap",function (req,res) {
+
+    res.render("dangnhap",{
+    });
+
+})
+// app.get("/demo1",function (req,res) {
+//     const search = req.query.search
+//     var sql="select * from products where Name like '%"+search+"%'";
+//     res.render("demo1",{
+//         search:search
+//        });
+//
+// })
+app.get("/demo1",function (req,res) {
+    const search = req.query.search
+ // var sql="select * from products where Name like '%"+search+"%'";
+    var sql_txt = "SELECT * FROM `products`";
+    conn.query(sql_txt,function (err,data) {
+        if (err) res.send("404 not found");
+        else {
+            var products = data;
+            var products1 = data;
+            var test = data[9];
+            res.send(data);
+            // res.render("demo1", {
+            //     "products": products,
+            //     "products1": products1,
+            //      search:search,
+            //      test:test,
+            // });
+        }
+    })
+})
+
+app.get("/test3",function (req,res) {
+
+    const sql_txt = "SELECT * FROM `products`";
+    conn.query(sql_txt,function (err,data) {
+        if(err) res.send("404 not found");
+        else{
+            var products = data;
+            var products1 = data;
+            res.render("test3",{
+                "products":products,
+                "products1":products1
+            });
+        }
+    })
+
+});
+// app.get("/test4",function (req,res) {
+//
+//     const sql_txt = "SELECT * FROM `products`";
+//     conn.query(sql_txt,function (err,data) {
+//         if(err) res.send("404 not found");
+//         else{
+//             var products = data;
+//             var products1 = data;
+//             res.send(data);
+//             // res.render("demo1",{
+//             //     "products":products,
+//             //     "products1":products1,
+//             //
+//             // });
+//         }
+//     })
+//
+// })
